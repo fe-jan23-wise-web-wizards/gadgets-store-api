7f760eeebcb0fe241877d7e061c9a4bd1de69616
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-08 18:44:00 +0300

    updated env config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 const config = {
-  env: { browser: true, es2020: true },
+  env: { node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
