5843afe3d2b63b0def4f63060ba8592189ee82f8
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-09 20:44:20 +0300

    added products routes

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import { router as phonesRouter } from './routes/phones';
+import { router as productsRouter } from './routes/products';
 
 export const app = express();
 
@@ -9,4 +10,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/phones', phonesRouter);
+app.use('/products', productsRouter);
 app.use('/static', express.static(path.join(__dirname, '../public')));
