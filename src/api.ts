7838460083d5148db2b1902eeae06a4e67383963
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-09 16:29:19 +0300

    added static files serving

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import { router as phonesRouter } from './routes/phones';
 
 export const app = express();
@@ -8,3 +9,4 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/phones', phonesRouter);
+app.use('/static', express.static(path.join(__dirname, '../public')));
