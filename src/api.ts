1088f988320a282ce03f7ebc5061cf6ec5f22a9c
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-12 18:52:24 +0300

    removed /phone endpoints

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
-import { router as phonesRouter } from './routes/phones';
 import { router as productsRouter } from './routes/products';
 
 export const app = express();
@@ -9,6 +8,5 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/phones', phonesRouter);
 app.use('/products', productsRouter);
 app.use('/static', express.static(path.join(__dirname, '../public')));
--- src/services/phones.ts
+++ src/services/phones.ts
@@ -1,44 +1,4 @@
 import { prisma } from "../database/prisma";
-import { SortBy } from '../types/SortBy';
-
-export const getAllPhones = (
-  page: number,
-  limit: number,
-  sort: SortBy) => {
-  const orderBy = {};
-  
-  switch (sort) {
-    case SortBy.PriceLowest:
-      Object.assign(orderBy, { price: 'asc' });
-      break;
-    case SortBy.PriceHighest:
-      Object.assign(orderBy, { price: 'desc' });
-      break;
-    case SortBy.Newest:
-      Object.assign(orderBy, { year: 'desc' });
-      break;
-    case SortBy.Oldest:
-      Object.assign(orderBy, { year: 'asc' });
-      break;
-    case SortBy.Default:
-    default:
-      Object.assign(orderBy, { name: 'desc' });
-      break;
-  }
-  
-  const phones = prisma.product.findMany({
-    where: {
-      category: 'phones',
-    },
-    skip: (page - 1) * limit,
-    take: limit,
-    orderBy: {
-      ...orderBy,
-    }
-  }); 
-
-  return phones;
-};
 
 export const getPhoneById = (id: string) => {
   return prisma.phone.findUnique({
